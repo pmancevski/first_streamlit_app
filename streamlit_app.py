646c99114fe84ed8921e7d57df2a088101a2a605
Pero Manchevski <pmancevskiwork@gmail.com>
2023-01-04 06:59:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,11 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+        return "Thanks for adding " + new_fruit
+
 # Read CSV file from snowflake AWS bucket
 my_fruit_list = pandas.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -61,8 +66,7 @@ if streamlit.button('Get Fruit List'):
 
 #user input
 add_my_fruit = streamlit.text_input('What fruit would you like to add??')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
-streamlit.stop()
-
-my_cur.execute("insert into fruit_load_list values ('from_streamlit')")
+if streamlit.button("Add a Fruit to the List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    backFromFun = insert_row_snowflake(add_my_fruit)
+    streamlit.text(backFromFun)
