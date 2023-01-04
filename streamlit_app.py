8296ad9f1e87c03499bde6c35f5e4af21354aa68
Pero Manchevski <pmancevskiwork@gmail.com>
2023-01-04 06:25:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 # Read CSV file from snowflake AWS bucket
 my_fruit_list = pandas.read_csv(
@@ -40,6 +41,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # To table view
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
