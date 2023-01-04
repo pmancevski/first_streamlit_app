28552ad1f68f2da6b3c6721b5214e0a090b0ff2e
Pero Manchevski <pmancevskiwork@gmail.com>
2023-01-04 04:46:05 +0100

    Update streamlit_app.py
    - Added user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ streamlit.dataframe(fruits_to_show)
 
 #header
 streamlit.header("Fruityvice Fruit Advice!")
+#user input
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # fruit API response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
